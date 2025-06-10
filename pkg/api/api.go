a8be4cfe640f7449ee3b2ed0b8c787e45ca66274
maykelxyz <mjtpediglorio@gmail.com>
2025-06-11 05:04:54 +0800

    [Feat] Fetch API url

---
+++ pkg/api/api.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"net/http"
+)
+
+func CheckAPIHeartbeat(url string) bool {
+	resp, err := http.Get(url)
+	if err != nil {
+		return false
+	}
+	defer resp.Body.Close()
+
+	return resp.StatusCode == http.StatusOK
+}
